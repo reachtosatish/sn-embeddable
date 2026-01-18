bb745197f969324a72512704ae7a62b92dce9ba3
Satish Yanamadala <satish.yanamadala@virginplus.ca>
2026-01-17 20:30:50 -0500

    Fix routing with basename for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
   }
 
   return (
-    <Router history={history}>
+    <Router history={history} basename="/sn-embeddable">
       <div id="app" className="d-flex flex-column h-100">
         <NavBar />
         <Container className="flex-grow-1 mt-5">
